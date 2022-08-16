3efab0bfe51293166042ef4d3f7ad062c2d5a964
LokeshTiwari004 <lokeshtiwari001vns@gmail.com>
2022-08-16 20:54:28 +0530

    ..

--- src/Components/ComponentC.js
+++ src/Components/ComponentC.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { UserConsumer } from './userContext'
+import React from "react";
+import { UserConsumer } from "./userContext";
 
 function ComponentC() {
   return (
@@ -10,7 +10,7 @@ function ComponentC() {
         }
       }
     </UserConsumer>
-  )
+  );
 }
 
-export default ComponentC
+export default ComponentC;
